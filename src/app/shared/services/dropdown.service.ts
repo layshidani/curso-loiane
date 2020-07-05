33641db1ac4459daf5f484a44c57571bedc37268
Lays Hidani <lays.hidani@accenture.com>
2020-07-05 18:38:24 -0300

    fix(DropdownService): fix type :bug:

--- src/app/shared/services/dropdown.service.ts
+++ src/app/shared/services/dropdown.service.ts
@@ -1,13 +1,13 @@
-
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { StateBR } from './../models/state.model';
 
 @Injectable()
 export class DropdownService {
   constructor(private http: HttpClient) { }
 
   getStates() {
-    return this.http.get('assets/data/states.json')
-      .map((res: Response) => res.json());
+    return this.http.get<StateBR[]>('assets/data/states.json');
   }
 }
