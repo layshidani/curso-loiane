6646ea9d1cd19afaced7a07d1f2e52588fd6189d
Lays Hidani <lays.hidani@accenture.com>
2020-07-23 15:12:02 -0300

    feat(courses): add courses list :sparkles:

--- http-requests/src/app/app-routing.module.ts
+++ http-requests/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cursos' },
+  { path: 'cursos', loadChildren: './courses/courses.module#CoursesModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- http-requests/src/app/courses/courses.module.ts
+++ http-requests/src/app/courses/courses.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesListComponent } from './courses-list/courses-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CoursesListComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule
