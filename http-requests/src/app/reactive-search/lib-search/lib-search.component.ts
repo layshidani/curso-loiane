ad6c73ff7afc7733d5b30afc36686536e3d818e3
Lays Hidani <lays.hidani@accenture.com>
2020-08-20 12:26:19 -0300

    feat(search): implements reactive search :sparkles:

--- http-requests/src/app/reactive-search/lib-search/lib-search.component.ts
+++ http-requests/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { tap, map } from 'rxjs/operators';
+import { tap, map, filter, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -13,26 +13,43 @@ export class LibSearchComponent implements OnInit {
 
   queryField = new FormControl();
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  readonly FIELDS = 'name,description,version,homepage';
   results$: Observable<any>;
   total: number;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.results$ = this.queryField.valueChanges
+      .pipe(
+        map(value => value.trim()),
+        filter(value => value.length > 1),
+        debounceTime(200),
+        distinctUntilChanged(),
+        // tap(value => console.log(value)),
+        switchMap(value => this.http.get(this.SEARCH_URL, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        })),
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results)
+      );
+  }
 
   onSearch() {
-    const fields = 'name,description,version,homepage';
     let value = this.queryField.value;
     if (value && (value = value.trim()) !== '') {
 
-      const params_ = {
-        search: value,
-        fields: fields
-      };
+      // const params_ = {
+      //   search: value,
+      //   fields: fields
+      // };
 
       let params = new HttpParams();
       params = params.set('search', value);
-      params = params.set('fields', fields);
+      params = params.set('fields', this.FIELDS);
 
       this.results$ = this.http
         .get(this.SEARCH_URL, { params })
