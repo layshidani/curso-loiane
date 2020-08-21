44ae3de4cca26bb11e5a0516e4f6b7c6e5b928d5
Lays Hidani <lays.hidani@accenture.com>
2020-08-21 11:30:07 -0300

    deploy com Firebase :rocket:

--- http-requests/src/app/app-routing.module.ts
+++ http-requests/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'cursos' },
+  { path: '', pathMatch: 'full', redirectTo: 'busca-reativa' },
   {
     path: 'cursos',
     loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
